c8122a3a9bb361db05f5bf72b404f025ce17a14a
Tea <cha9n.1488@gmail.com>
2025-09-25 14:16:10 +0300

    Initial commit from Spring Initializr

---
+++ src/main/java/mate/academy/web_intro_bookstore/WebIntroBookstoreApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.web_intro_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIntroBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebIntroBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mate/academy/web_intro_bookstore/WebIntroBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.web_intro_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIntroBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
