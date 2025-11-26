a15818f3944c62e07e4ab096ae0dca59678fae1c
Tea <cha9n.1488@gmail.com>
2025-11-26 14:59:15 +0200

    2344

--- src/main/java/mate/academy/webintrobookstore/repository/book/spec/TitleSpecificationProvider.java
+++ src/main/java/mate/academy/webintrobookstore/repository/book/spec/TitleSpecificationProvider.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TitleSpecificationProvider implements SpecificationProvider<Book> {
+
     @Override
     public String getKey() {
         return "title";
