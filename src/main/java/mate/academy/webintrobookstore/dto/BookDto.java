1e3a0eb895e4441b39b2f7f0295ec7428f739844
Tea <cha9n.1488@gmail.com>
2026-01-06 15:06:05 +0200

    GlobalExceptionHandler and data validation DONE

--- src/main/java/mate/academy/webintrobookstore/dto/BookDto.java
+++ src/main/java/mate/academy/webintrobookstore/dto/BookDto.java
@@ -1,19 +1,27 @@
 package mate.academy.webintrobookstore.dto;
 
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class BookDto {
+    private Long id;
 
+    @NotNull
     private String title;
 
+    @NotNull
     private String author;
 
     private String isbn;
 
+    @NotNull
+    @Min(0)
     private BigDecimal price;
 
     private String description;
